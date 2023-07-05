86325dfeccbfb28c89c0fda8bd226871d9cddabc
ChiefLopes <chieflopesekede@gmail.com>
2023-07-05 11:43:31 +0100

    added javascript features, by changing the letters in the lable to an array and adding a span to each

---
+++ main.js
@@ -0,0 +1,14 @@
+const labels = document.querySelectorAll("label");  //This returns a nodelist of all labels in the html document.
+// And now because its a nodelist, we can go ahead and use forEach on it.
+labels.forEach(label => {
+    label.innerHTML = label.innerText // this sets the innerHTMl(label) to match with the innerText (Email).
+        .split("")   // We now split the letters into an arrayusing the .split() array method.
+        .map((letter, idx) => `<span>${letter}</span>`) // Here, we map thru the letters, creating a span around each of them.
+        .join(' ')  // this returns the letters to a single element separated by ' '
+        
+})
+
+
+console.log(labels);
+
+
