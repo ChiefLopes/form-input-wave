b3fe050e0f9b218df3fe454b2219682de87cd1af
ChiefLopes <chieflopesekede@gmail.com>
2023-07-05 12:28:11 +0100

    added css features for the input and label span

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ labels.forEach(label => {
     label.innerHTML = label.innerText // this sets the innerHTMl(label) to match with the innerText (Email).
         .split("")   // We now split the letters into an arrayusing the .split() array method.
         .map((letter, idx) => `<span>${letter}</span>`) // Here, we map thru the letters, creating a span around each of them.
-        .join(' ')  // this returns the letters to a single element separated by ' '
+        .join('')  // this returns the letters to a single element separated by ' '
         
 })
 
