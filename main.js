a437c3a61ed9b7edf039b4cd410a74ffdbba6606
ChiefLopes <chieflopesekede@gmail.com>
2023-07-05 13:06:28 +0100

    added css features to the script by adding an inline styling to the mapped leters so that transition delay goes from 50ms upwards

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const labels = document.querySelectorAll(".form-control label");  //This returns
 labels.forEach(label => {
     label.innerHTML = label.innerText // this sets the innerHTMl(label) to match with the innerText (Email).
         .split("")   // We now split the letters into an arrayusing the .split() array method.
-        .map((letter, idx) => `<span>${letter}</span>`) // Here, we map thru the letters, creating a span around each of them.
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`) // Here, we map thru the letters, creating a span around each of them.
         .join('')  // this returns the letters to a single element separated by ' '
         
 })
